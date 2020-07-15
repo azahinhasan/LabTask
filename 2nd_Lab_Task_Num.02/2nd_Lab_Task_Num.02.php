8b7887082d0938f74668f8c7d9bc8d96723dcaa5
A H Zahin <azahinhasan@gmail.com>
2020-07-15 21:49:47 +0600

    Add files via upload

---
+++ 2nd_Lab_Task_Num.02/2nd_Lab_Task_Num.02.php
@@ -0,0 +1,112 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <style>
+        .error {
+            color: #FF0000;
+        }
+
+        #date {
+            width: 27px;
+        }
+
+        #form {
+            text-align: left;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+
+    $nameErr = $dBirth = $Gender = $Degree =  $bGroup = $email = $emailErr = "";
+    $name = $dd = $mm = $yy = $genderr = $degree = $group = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["name"])) {
+            $nameErr = "Name is required";
+        } else {
+            $name = test_input($_POST["name"]);
+            if (!preg_match("/^[a-zA-Z  _]*$/", $name)) {
+                $nameErr = "Only letters  space allowed";
+            }
+        }
+        if (empty($_POST["email"])) {
+            $emailErr = "Email is required";
+        } else {
+            $email = test_input($_POST["email"]);
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $emailErr = "Invalid email format";
+            }
+        }
+        if (empty($_POST["dd"]) || empty($_POST["mm"]) || empty($_POST["yy"])) {
+            $dBirth = "Date Of  Birth is Empty";
+        } else {
+            $dd = test_input($_POST["dd"]);
+            $mm = test_input($_POST["mm"]);
+            $yy = test_input($_POST["yy"]);
+            if (!preg_match("/^[1-31]*$/", $dd)) {
+                $dBirth = "Only this format (dd: 1-31, mm: 1-12, yyyy:1953-1998) allowed";
+            } else if (!preg_match("/^[1-12]*$/", $mm)) {
+                $dBirth = "Only this format (dd: 1-31, mm: 1-12, yyyy:1953-1998) allowed";
+            } else if ($yy >= 1953) {
+                if ($yy < 1998) {
+                    $dBirth = $yy;
+                }
+            }
+        }
+        if (empty($_POST["female"]) && empty($_POST["male"]) && empty($_POST["other"])) {
+            $Gender = "At Last One Gender is required";
+        }
+        if (empty($_POST["class1"]) && empty($_POST["class2"]) && empty($_POST["class3"])) {
+            $Degree = "At Last One Degree is required";
+        }
+        if (empty($_POST["BloodG"])) {
+            $bGroup = "At Last One BloodG is required";
+        }
+    }
+
+    function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    ?>
+
+    <form id="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        Name: <input type="text" name="name" value=<?php echo $name ?>>
+        <span class="error">* <?php echo $nameErr; ?></span>
+        <br><br>
+        Email: <input type="text" name="email" value=<?php echo $email ?>>
+        <span class="error">* <?php echo $emailErr; ?></span>
+        <br><br>
+        Date of Birth: <input type="text" name="dd" id="date" value=<?php echo $dd ?>> / <input type="text" name="mm" id="date" value=<?php echo $mm ?>> /<input type="text" name="yy" id="date" value=<?php echo $yy ?>>
+        <span class="error">* <?php echo $dBirth; ?></span>
+        <br><br>
+        Gender:
+        <input type="radio" name="female" <?php echo (isset($_POST['female']) == 'checked') ?  'checked' : ''; ?>>Female
+        <input type="radio" name="male" <?php echo (isset($_POST['male']) == 'checked') ?  'checked' : ''; ?>>Male
+        <input type="radio" name="other" <?php echo (isset($_POST['other']) == 'checked') ?  'checked' : ''; ?>>Other
+        <span class="error">* <?php echo $Gender; ?></span>
+        <br><br>
+        Degree:
+        <input type="checkbox" name="class1" <?php echo (isset($_POST['class1']) == 'checked') ?  'checked' : ''; ?>>HSC
+        <input type="checkbox" name="class2" <?php echo (isset($_POST['class2']) == 'checked') ?  'checked' : ''; ?>>BSc
+        <input type="checkbox" name="class3" <?php echo (isset($_POST['class3']) == 'checked') ?  'checked' : ''; ?>>MSc
+        <span class="error">* <?php echo $Degree; ?></span>
+        <br><br>
+        Blood Group:
+        <select name="BloodG" id="bGroup">
+            <option name="A+">A+</option>
+            <option name="B+">B+</option>
+        </select>
+        <span class="error">* <?php echo $bGroup; ?></span>
+        <br><br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+</body>
+
+</html>
