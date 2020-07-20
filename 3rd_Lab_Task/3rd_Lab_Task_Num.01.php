0245b257b7b1233714e8a560d0464e4673d47b12
Ashhak Hasan Zahin <azahinhasan@gmail.com>
2020-07-20 10:05:39 +0600

    1 and 2

---
+++ 3rd_Lab_Task/3rd_Lab_Task_Num.01.php
@@ -0,0 +1,69 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <style>
+        .error {
+            color: #FF0000;
+        }
+
+        #date {
+            width: 27px;
+        }
+
+        #form {
+            text-align: left;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+
+    $nameErr = $dBirth = $Gender = $password =  $passErr = $name = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["name"])) {
+            $nameErr = "UserName is required";
+        } else {
+            $name = test_input($_POST["name"]);
+            if (!preg_match("/^[a-zA-Z  _]*$/", $name)) {
+                $nameErr = "Only letters  space allowed";
+            } elseif (strlen($_POST["name"]) < 2) {
+                $nameErr = "Must Contain 2 characters";
+            }
+        }
+        if (empty($_POST["password"])) {
+            $passErr = "Email is required";
+        } else {
+            $password = test_input($_POST["password"]);
+            if (strlen($_POST["password"]) > 8) {
+                $passErr = "Must Contain less then 8 characters";
+            } elseif (!preg_match("/[@#$%]*$/", $password)) {
+                $passErr = "Password must contain at least one of the special characters (@, #, $,%)";
+            }
+        }
+    }
+
+    function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    ?>
+
+    <form id="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        User Name: <input type="text" name="name" value=<?php echo $name ?>>
+        <span class="error">* <?php echo $nameErr; ?></span>
+        <br><br>
+        Password: <input type="password" name="password" value=<?php echo $password ?>>
+        <span class="error">* <?php echo $passErr; ?></span>
+        <br><br>
+        <input type="submit" name="submit" value="Submit">
+        <spn> <a href="#">Forgot Password?</a></spn>
+    </form>
+</body>
+
+</html>
---
+++ New folder/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <style>
+    .currentLink {
+      color: #640200;
+      background-color: #000000;
+    }
+
+    .topnav {
+      background-color: black;
+      color: white;
+    }
+
+    .topnav a.news {
+      background-color: #4CAF50;
+      color: white;
+    }
+  </style>
+</head>
+
+<body>
+  <?php include("a.html"); ?>
+  <h1>Hi, I am Zahin Hasan. I am testing Navigation.</h1>
+</body>
+
+</html>
